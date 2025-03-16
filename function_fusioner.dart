f4c62a253d1ff2f250369fa179a87b62d3ed8413
R-Vaishnav-Raj <vaishnavraj029@gmail.com>
2025-03-16 22:06:58 +0530

    used lambda functions

--- function_fusioner.dart
+++ function_fusioner.dart
@@ -3,15 +3,13 @@ void processList(List<int> nums,int Function(int) operation){
   nums=nums.map(operation).toList();
   print("The List after operation is $nums");
 }
-int square(int num)=>num*num;
-int cube(int num)=>num*num*num;
+//int square(int num)=>num*num;
+//int cube(int num)=>num*num*num;
 void main(){
 
   List<int> numbers = [1,2,3,4,5];
 
-  processList(numbers, square);
-  processList(numbers, cube);
-
-
+  processList(numbers, (int num)=>num*num);
+  processList(numbers, (int num)=>num*num*num);
 
 }
